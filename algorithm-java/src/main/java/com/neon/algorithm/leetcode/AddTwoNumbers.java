56d81abfab7cd7a5bcc25d3fa437c2259d2690d7
neon_home <niki.kudrin@gmail.com>
2020-12-06 13:50:20 +0200

    leetcode add two numbers task

---
+++ algorithm-java/src/main/java/com/neon/algorithm/leetcode/AddTwoNumbers.java
@@ -0,0 +1,89 @@
+package com.neon.algorithm.leetcode;
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+/**
+ * https://leetcode.com/problems/add-two-numbers/
+ * <p>
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Example:
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * <p>
+ * Solution: for support very big numbers (more then long and BigInt) we should implement addition by a column method
+ * </p>
+ */
+public class AddTwoNumbers {
+    public static ListNode addTwoNumbers(ListNode firstList, ListNode secondList) {
+        if (firstList == null || secondList == null)
+            throw new IllegalArgumentException("Lists should not be null");
+
+        int tempNumber = 0;
+        ListNode sumList = null, currentSumNode = null;
+        ListNode firstPointer = firstList, secondPointer = secondList;
+        int firstNumber = 0, secondNumber = 0;
+
+        do {
+            if (firstPointer == null)
+                firstNumber = 0;
+            else
+                firstNumber = firstPointer.val;
+
+            if (secondPointer == null)
+                secondNumber = 0;
+            else
+                secondNumber = secondPointer.val;
+
+            if (firstNumber > 9 || secondNumber > 9)
+                throw new IllegalArgumentException("Digit of the number should be less then 9");
+
+            var sum = firstNumber + secondNumber + tempNumber;
+
+            if (sum > 9) {
+                tempNumber = 1;
+                sum = sum % 10;
+            } else {
+                tempNumber = 0;
+            }
+
+            if (sumList == null) {
+                sumList = new ListNode(sum);
+                currentSumNode = sumList;
+            } else {
+                currentSumNode.next = new ListNode(sum);
+                currentSumNode = currentSumNode.next;
+            }
+
+            if (firstPointer != null) {
+                firstPointer = firstPointer.next;
+            }
+            if (secondPointer != null) {
+                secondPointer = secondPointer.next;
+            }
+
+            if (firstPointer == null && secondPointer == null && tempNumber > 0)
+                currentSumNode.next = new ListNode(tempNumber);
+        }
+        while (firstPointer != null || secondPointer != null);
+
+        return sumList;
+    }
+
+    private static boolean hasNext(ListNode list) {
+        return list.next != null;
+    }
+}
---
+++ algorithm-java/src/test/java/com/neon/algorithm/leetcode/AddTwoNumbersTest.java
@@ -0,0 +1,85 @@
+package com.neon.algorithm.leetcode;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.javatuples.Pair;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AddTwoNumbersTest {
+    private ListNode convertLongToList(long number) {
+        var strNumber = Long.valueOf(number).toString();
+        ListNode sumList = null, currentNode = null;
+
+        var charArray = strNumber.toCharArray();
+
+        for (var index = charArray.length - 1; index >= 0; index--) {
+            int digit = Character.getNumericValue(charArray[index]);
+
+            if (sumList == null) {
+                sumList = new ListNode(digit);
+                currentNode = sumList;
+            } else {
+                currentNode.next = new ListNode(digit);
+                currentNode = currentNode.next;
+            }
+        }
+
+        return sumList;
+    }
+
+    private String convertListToString(ListNode list) {
+        StringBuilder builder = new StringBuilder();
+
+        var currentNode = list;
+
+        while (currentNode != null) {
+            builder.append(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        var reversedChars = builder.toString().toCharArray();
+        ArrayUtils.reverse(reversedChars);
+
+        return String.valueOf(reversedChars);
+    }
+
+    @TestFactory
+    public Stream<DynamicTest> addTwoNumbers_DynamicTests() {
+        var testData = Arrays.asList(
+                new Pair<Long, Long>(0L, 0L),
+                new Pair<Long, Long>(1L, 1L),
+                new Pair<Long, Long>(8L, 44L),
+                new Pair<Long, Long>(44L, 8L),
+
+                new Pair<Long, Long>(14L, 20L),
+                new Pair<Long, Long>(342L, 465L),
+                new Pair<Long, Long>(465L, 342L),
+
+                new Pair<Long, Long>(999L, 999L),
+                new Pair<Long, Long>(10000989890000000L, 2323232323232232L)
+        );
+
+        return testData.stream()
+                .map(numberPair -> DynamicTest.dynamicTest("addTwoNumbers_DynamicTests",
+                        () -> {
+                            var firstNumber = numberPair.getValue0();
+                            var secondNumber = numberPair.getValue1();
+                            var firstList = convertLongToList(firstNumber);
+                            var secondList = convertLongToList(secondNumber);
+                            var expected = firstNumber + secondNumber;
+
+                            var sumList = AddTwoNumbers.addTwoNumbers(firstList, secondList);
+                            var actual = Long.valueOf(convertListToString(sumList));
+
+                            assertThat(expected)
+                                    .describedAs(String.format("First: %s Second: %s", firstNumber, secondNumber))
+                                    .isEqualTo(actual);
+                        }
+                ));
+    }
+}
